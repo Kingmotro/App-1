0d9e7a4d483dafa3ffb447ee3c60e380c2211f00
Andrew Aubury <andrewaubury1@gmail.com>
2018-09-02 14:56:52 +0100

    Starting on app

---
+++ MinePoS/MinePoS/SplashPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Timers;
+
+namespace MinePoS
+{
+	public partial class SplashPage : ContentPage
+	{
+		public SplashPage()
+		{
+			InitializeComponent();
+            // this.countLabel.Text = "0";
+            DelayActionAsync(5000, RunEverySecond);
+        }
+
+       
+        private void RunEverySecond() {
+           
+
+        }
+
+        public async Task DelayActionAsync(int delay, Action action)
+        {
+
+            await Task.Delay(delay);
+            action();
+        }
+    }
+}
